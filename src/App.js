9baaba66e6f6ebc48e5992a83c43e2a1dde62ca4
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-03 15:40:57 +0100

    scd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { useState } from "react";
 import Counter from "./Components/Counter";
-import Reset from "./Components/Reset";
 
 import logo from "./img/calc.png";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  // const [addcounter, setAddCounter] = useState(1);
+
+  const tab = [];
+
   return (
     <div className="App">
       <header>
@@ -15,14 +18,41 @@ function App() {
         </div>
         <p>React counter V2</p>
       </header>
+      <main>
+        <section>
+          <button
+            className="addcounter"
+            onClick={() => {
+              for (let i = 0; i <= 3; i++) {
+                tab.push(<Counter counter={counter} setCounter={setCounter} />);
+              }
+            }}
+          >
+            Add counter
+          </button>
+        </section>
 
-      <section className="sectionOne">
-        <div className="component">
-          <Counter counter={counter} setCounter={setCounter} />
-          <Reset setCounter={setCounter} />
-        </div>
-      </section>
+        <section className="sectionOne">
+          <div className="component">
+            <Counter counter={counter} setCounter={setCounter} />
+          </div>
+        </section>
 
+        {/* {addcounter === 1 ? (
+          <section className="sectionOne">
+            <div className="component">
+              <Counter counter={counter} setCounter={setCounter} />
+            </div>
+          </section>
+        ) : (
+          <section className="sectionOne">
+            <div className="component">
+              <Counter counter={counter} setCounter={setCounter} />
+              <Counter counter={counter} setCounter={setCounter} />
+            </div>
+          </section>
+        )} */}
+      </main>
       <footer>
         <p>
           Made with <span> React </span> at <span> Le Reacteur </span> by{" "}
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,33 +1,47 @@
 const Counter = ({ counter, setCounter }) => {
   return (
     <div className="buttons">
-      <div className="firstbutton">
-        <button
-          className="min"
-          onClick={() => {
-            setCounter(counter - 1);
-          }}
-        >
-          -
-        </button>
-      </div>
+      <section className="one">
+        <div className="firstbutton">
+          <button
+            className="min"
+            onClick={() => {
+              setCounter(counter - 1);
+            }}
+          >
+            -
+          </button>
+        </div>
 
-      <div>
-        <button className="coun">
-          <span>{counter}</span>
-        </button>
-      </div>
+        <div>
+          <button className="coun">
+            <span>{counter}</span>
+          </button>
+        </div>
 
-      <div className="thirdbutton">
-        <button
-          className="add"
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+        <div className="thirdbutton">
+          <button
+            className="add"
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            +
+          </button>
+        </div>
+      </section>
+      <section className="two">
+        <div className="secondbutt">
+          <button
+            className="reset"
+            onClick={() => {
+              setCounter(0);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
